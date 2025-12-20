160a019531fef71021cd33e16ecda4b5bf0c1628
thesmokecurtainguy <thesmokecurtainguy@gmail.com>
2025-12-19 21:25:59 -0500

    Fix build: Add Prisma client generation to build scripts and update package.json

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -168,3 +168,4 @@ export default function Navbar({ user }: NavbarProps) {
   );
 }
 
+
--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -11,3 +11,4 @@ export default function Providers({ children }: ProvidersProps) {
   return <SessionProvider>{children}</SessionProvider>;
 }
 
+
